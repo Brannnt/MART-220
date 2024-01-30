774f87c37e5cbae1992a202ebde5f1aebd9c2a40
Brannt Fahrney <123214814+Brannnt@users.noreply.github.com>
2024-01-29 23:16:51 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+  //meat
+fill(150,75,0);
+rect(60,185,280,70);
+  
+  //onion
+fill(200,200,200);
+rect(70,160,260,25);
+  
+    //tomato
+fill(200,50,50);
+ellipse(200,140,250,60);
+  
+//top bun
+fill(200,120,70);
+rect(50,30,300,100);
+  
+//lettuce
+fill(100,200,100);
+triangle(50,130, 350, 130, 200, 150);
+
+//cheeze
+fill(240,240,20);
+triangle(50,185, 350, 185, 200, 220);
+
+//bottom bun
+fill(200,120,70);
+rect(50,255,300,80);
+
+  } 
